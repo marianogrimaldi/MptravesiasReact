2d7c2e336bd3be0bc77a738e613c9a010e62a6b6
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-10-12 00:24:33 -0300

    aplicacion correcta de mq

--- src/Components/Tour/Tour.jsx
+++ src/Components/Tour/Tour.jsx
@@ -1,5 +1,6 @@
 import "./Tour.scss"
 import tour from "../../assets/tourPrivado.jpg"
+import tourMobile from "../../assets/tourPrivadoMobile.jpg"
 import icasa from "../../assets/logoMarcas/icasa.png"
 import video from "../../assets/logoVideo.png"
 import { useTranslation } from "react-i18next"
@@ -20,7 +21,8 @@ const Tour = () =>{
                 <div className="trianguloNos3"></div>
             </div>
             <a href="https://www.icasamotos.com.ar/" target="_blank"><img src={icasa} className="logoIcasaNos" alt="Logo Icasa" /></a>
-            <img src={tour} className="imgTour" alt="Grupo Pivado" />
+            <img src={tour} className="imgTour" alt="Grupo Privado" />
+            <img src={tourMobile} className="imgTourMobile" alt="Grupo Privado" />
             <a href="https://www.instagram.com/p/CtJmaRHMThU/" target="blank"><img src={video} className="logoVideo" alt="Logo Video" /></a>
         </div>
     )
