d9c6c3c1ccbfc1b1d00d67bd207703572faf53cb
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2024-02-06 09:33:26 -0300

    cambios en boton de lenguaje

--- src/Components/Traductor/Traductor.jsx
+++ src/Components/Traductor/Traductor.jsx
@@ -11,21 +11,24 @@ import { useTranslation } from 'react-i18next';
 const Traductor = () => {
 
     const { t, i18n } = useTranslation();
-    const cambiar = (lan)=>{
-        localStorage.setItem("lang",lan);
+    const cambiar = (lan) => {
+        localStorage.setItem("lang", lan);
         i18n.changeLanguage(lan);
     }
     return (
-        
-            <div className="flags">
-                <img onClick={()=>{cambiar("es")}} src={España} alt="Bandera de España" />
-                <img onClick={()=>{cambiar("en")}}src={Inglaterra} alt="Bandera de Inglaterra" />
-                <img onClick={()=>{cambiar("port")}}src={Portugal} alt="Bandera de Portugal" />
-                <img onClick={()=>{cambiar("ger")}}src={Alemania} alt="Bandera de Alemania" />
-                <img onClick={()=>{cambiar("fr")}}src={Francia} alt="Bandera de Francia" />   
+
+        <div className="flags">
+            <div class="dropdown">
+                <button class="boton-desplegable">{t("Idioma.titulo")}</button>
+                <div class="contenido-dropdown">
+                <a href="#"><img onClick={() => { cambiar("es") }} src={España} alt="Bandera de España" /></a>
+                <a href="#"><img onClick={() => { cambiar("en") }} src={Inglaterra} alt="Bandera de Inglaterra" /></a>
+                <a href="#"><img onClick={() => { cambiar("port") }} src={Portugal} alt="Bandera de Portugal" /></a>
+                <a href="#"><img onClick={() => { cambiar("ger") }} src={Alemania} alt="Bandera de Alemania" /></a>
+                <a href="#"><img onClick={() => { cambiar("fr") }} src={Francia} alt="Bandera de Francia" /></a>
+                </div>
             </div>
-           
-    
+        </div>
     )
 }
 
